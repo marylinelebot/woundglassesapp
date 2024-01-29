0e6bda4b6d8fd1e30e2e4687e4fa35cbc16930a4
marylinelebot <157613062+marylinelebot@users.noreply.github.com>
2024-01-29 10:30:23 +0100

    SessionManager.java

--- app/src/main/java/com/example/myapplication/SessionManager.java
+++ app/src/main/java/com/example/myapplication/SessionManager.java
@@ -7,6 +7,7 @@ public class SessionManager {
 
     private static final String PREF_NAME = "MyAppSession";
     private static final String KEY_IS_LOGGED_IN = "isLoggedOut";
+    private static final String KEY_USER_EMAIL = "userEmail";
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
@@ -22,6 +23,17 @@ public class SessionManager {
     public void setLoggedIn(boolean isLoggedIn) {
         editor.putBoolean(KEY_IS_LOGGED_IN, isLoggedIn);
         editor.apply();
+    }
+
+    // Stock user email
+    public void setUserEmail(String email) {
+        editor.putString(KEY_USER_EMAIL, email);
+        editor.apply();
+    }
+
+    // Retrieve user email
+    public String getUserEmail() {
+        return sharedPreferences.getString(KEY_USER_EMAIL, null);
     }
 
     // Verify if user is logged in
